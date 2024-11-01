19a089367358f5b56147bb7fb78f1724171b95c5
Smithomness <162057546+Smithomness@users.noreply.github.com>
2024-10-31 22:36:11 -0700

    Add files via upload

---
+++ img scroller test.js
@@ -0,0 +1,20 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollBar,scrollLeft = evt.deltaY;
+});
+
+nextBtn.addEventListener("click", ()=>{
+    scrollContainer.scrollLeft += 900;
+});
+backBtn.addEventListener("click", ()=>{
+    scrollContainer.scrollLeft -= 900;
+});
+
+
+
+
+
